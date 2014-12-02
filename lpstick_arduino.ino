4365619de4937e524110b5c31098f6b625814a50
cybervinc <cybervinc@gmail.com>
2014-12-02 19:47:51 +0100

    Added a button

--- lpstick_arduino.ino
+++ lpstick_arduino.ino
@@ -11,18 +11,11 @@ void setup()
 {
  // Open serial communications and wait for port to open:
   Serial.begin(57600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   strip.setBrightness(30);
 
   Serial.print("Initializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin 
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
-  // or the SD library functions will not work. 
    pinMode(10, OUTPUT);
    
   if (!SD.begin(10)) {
@@ -31,7 +24,7 @@ void setup()
   }
   Serial.println("initialization done.");
   
-  while(lp){
+  while(lp && digitalRead(2){
   myFile = SD.open("data.dat");
   if (myFile) {
     int i=0;
@@ -48,15 +41,15 @@ void setup()
     //delay(pause);
     // close the file:
     myFile.close();
-  } else {
+    } 
+    else {
   	// if the file didn't open, print an error:
-    Serial.println("error opening dat.dat");
-    for(int i=0; i<144; i++){
+      Serial.println("error opening dat.dat");
+      for(int i=0; i<144; i++){
       strip.setPixelColor(i,0,0,0);
+      }
     }
   }
-  }
-
 }
 
 void loop()
